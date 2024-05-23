c343f2c12630da1a9cfec9dc129d51b85cf5b836
mariuszo52 <the506miz@gmail.com>
2024-05-23 18:57:52 +0200

    update MainPage.js

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,33 +1,93 @@
 import {useState} from "react";
 import axios from "axios"
 
-function MainPage(){
+
+function MainPage() {
     const [url, setUrl] = useState(null)
-    const [message, setMessage] = useState(null)
-    const token = "EAAF16jKZAliIBO350v0Xyiv5DOKSEbsrEr3sejh6h2ROWyH9wYzZAegynYZARSZBzDM32qpRQxEaZBR2lbGhLAmpkAGneUsDRR3iHArdVwXkQaQCGWpp1hkJQJOBVB76IQCjV1C5IovmMZBQ2f5GeBVWA7ByivKcisCqQYxhTW9qQBgJQfQQr3jpLGyEaZBJQ4IDuCk59FFbfrybzmqja7DiDcZD"
+    const [articleUrl, setArticleUrl] = useState(null)
+    const [spanishBody, setSpanishBody] = useState(null)
+    const [polishBody, setPolishBody] = useState("")
+    const [pass, setPass] = useState(null)
+    const [errors, setErrors] = useState("")
+    const token = "EAAF16jKZAliIBO3yIssTAIeCjU8FIFBoYBo1wERgVJNsmAFJW2QsktL3GxMHDZAzowyYGZCJumlKdc1ZAzVZAj9dTIVqlM4QlfChFxXNDUtKeELA2rYgWc3vkt4hDVZAceFttQQmB0jRof3y8XrSBF4Hc0eO6yvbaDOImGpFN19F1roeYQ4ZB8q4fyk8XelKp0ZD"
     const sendPostUrl = "https://graph.facebook.com/107046711659175/photos"
-    function onFormSubmit(event) {
+    const translateApiUrl = "https://api.openai.com/v1/chat/completions"
+
+
+
+    function scrapArticle(event) {
         event.preventDefault()
-        let data ={
-            url:url,
-            message:message,
-            access_token: token
+        let params ={
+            url: articleUrl
         }
-        axios.post(sendPostUrl, data )
-            .then(response => console.log(response.data))
-            .catch(error => console.log(error))
+
+        axios.post("http://localhost:8080/scrap", null, {params: params})
+            .then(response => {
+                setSpanishBody(response.data.body)
+                setUrl(response.data.photoUrl)
+            })
+            .catch(err => setErrors(err.response.data))
+
     }
 
-    return(
+    function onFormSubmit(event) {
+        event.preventDefault()
+        let data = {
+            url: url,
+            message: polishBody,
+            access_token: token
+        }
+        axios.post(sendPostUrl, data)
+            .then(response => console.log(response.data))
+            .catch(err => setErrors(err.response.data))
+
+    }
+
+    function translate(event) {
+        event.preventDefault()
+
+        let data = {
+            model: "gpt-4o",
+            messages: [
+                {
+                    role: "user",
+                    content: "Translate the following text to Polish: " + spanishBody
+                }],
+        }
+        let headers = {
+            Authorization: process.env.REACT_APP_GPT_API_KEY
+        }
+        axios.post(translateApiUrl, data, {headers: headers})
+            .then(response => setPolishBody(response.data.choices[0].message.content))
+            .catch(err => setErrors(err.response.data))
+    }
+
+
+    return (
         <div className={"main-div"}>
+            <p>SCRAPOWANIE</p>
+            <form onSubmit={scrapArticle}>
+                <label>PODAJ URL ARTYKUŁU</label>
+                <input onChange={event => setArticleUrl(event.target.value)}/>
+                <button type={"submit"}>SCRAPUJ ARTYKUŁ</button>
+            </form>
+            <form onSubmit={translate}>
+                <textarea value={spanishBody} onChange={event => setSpanishBody(event.target.value)}></textarea>
+                <button type={"submit"}>TŁUMACZ AI</button>
+            </form>
+
             <form onSubmit={onFormSubmit}>
                 <label>Url</label>
-                <input required={true} onChange={event=> setUrl(event.target.value)}/>
+                <input required={true} value={url} onChange={event => setUrl(event.target.value)}/>
                 <label>Message</label>
-                <textarea  required={true} onChange={event=> setMessage(event.target.value)}/>
+                <textarea required={true} value={polishBody} onChange={event => setPolishBody(event.target.value)}/>
+                <label>Password</label>
+                <input required={true} onChange={event => setPass(event.target.value)}/>
                 <button type={"submit"}>Prześlij</button>
             </form>
+            <p>ERRORS: {errors}</p>
         </div>
     )
 }
+
 export default MainPage;
